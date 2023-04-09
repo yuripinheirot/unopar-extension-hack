ba1022540f89091ad6f0c92e7754fc48b88f9c58
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-04-09 10:13:21 -0400

    feat: add remove keys

--- content.js
+++ content.js
@@ -4,7 +4,16 @@ document.removeEventListener('visibilitychange', handleVisibilityChange)
 window.removeEventListener('blur', handleWindowBlur)
 window.removeEventListener('focus', handleWindowFocus)
 
+window.removeEventListener('keydown', handleKeyDown)
+window.removeEventListener('keypress', handleKeyPress)
+window.removeEventListener('keyup', handleKeyUp)
+
 function handleVisibilityChange() {}
 function handleWindowBlur() {}
 function handleWindowFocus() {}
+
+function handleKeyDown() {}
+function handleKeyPress() {}
+function handleKeyUp() {}
+
 document.getElementsByTagName('video')[0].playbackRate = 16
