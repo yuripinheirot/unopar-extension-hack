c998d48aa06167576dcc925a6c862253f1ddfdc1
Yuri Pinheiro <yuripinheirot@gmail.com>
2023-04-09 09:43:56 -0400

    feat: add remove track events

---
+++ content.js
@@ -0,0 +1,7 @@
+document.removeEventListener('visibilitychange', handleVisibilityChange)
+window.removeEventListener('blur', handleWindowBlur)
+window.removeEventListener('focus', handleWindowFocus)
+
+function handleVisibilityChange() {}
+function handleWindowBlur() {}
+function handleWindowFocus() {}
